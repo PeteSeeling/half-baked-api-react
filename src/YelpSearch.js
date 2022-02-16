f9595974a3ba90860d365e0312a7036dd4e5571b
Pete Seeling <peteseeling@gmail.com>
2022-02-15 18:55:54 -0600

    yelp .env created

--- src/YelpSearch.js
+++ src/YelpSearch.js
@@ -4,10 +4,18 @@ import { search } from 'superagent';
 export default function YelpSearch() {
     // you'll need to track your yelp search results, the loading state, and a form field for location with a default value.
   const [businesses, setBusinesses] = useState([]);
-  const [search, setSearch] = useState('Detroit')
+  const [search, setSearch] = useState('Detroit');
+  const [loading, setLoading] = useState('false');
 
   async function handleYelpSubmit(e) {
     e.preventDefault();
+
+    setLoading(true);
+
+    const response = await fetch();
+    const json = await response.json();
+    setBusinesses(json);
+    setLoading(false);
   
     // set the loading state to true
     // use fetch to make a request to your netlify yelp function. Be sure to pass the search query as a query param in the URL
@@ -18,8 +26,12 @@ export default function YelpSearch() {
   return (
     <section className='yelp'>
       {/* make the fetch on submit */}
-      <form>
+      <form onSubmit={handleYelpSubmit}>
         Search yelp for a city
+        <label>
+          <input value='detroit or usa' onChange={(e => setSearch(e.target.value))} />
+        </label>
+      
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
         <button>Search yelp</button>
       </form>
